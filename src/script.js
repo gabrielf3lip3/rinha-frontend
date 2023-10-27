3b0c527506eab2f104ee932b82e4ec418e1728a2
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2023-10-27 20:07:30 -0300

    start to make the sitebeaultiful

--- src/script.js
+++ src/script.js
@@ -1,22 +1,34 @@
 import { jsonToTree } from "./worker.js"
 
 jQuery(() => {    
-  $(document).on('click' ,'li.test', function(e) {
-    alert("Clicou")
-  })
-
-  const input = document.getElementById("input")
-
-  input.addEventListener("change", (e) => {
+  $('#input').on("change", (e) => {
     const file = e.target.files[0]
     const reader = new FileReader()
+    const name = file.name
 
     reader.onload = (e) => {
-      const json = JSON.stringify(JSON.parse(e.target.result), null, 2)
-      const lines = json.split("\n")
+
+      try {
+        const json = JSON.stringify(JSON.parse(e.target.  result), null, 2)
+        const lines = json.split("\n")
       
-      jsonToTree(lines)
+        jsonToTree(lines)
+        hideElements(name)
+      }
+      catch(err) {
+        if(err instanceof SyntaxError)
+          $('#error').show()
+        else
+          hideElements()
+      }
     }
     reader.readAsText(file, "UTF-8")
   });
 })
+
+function hideElements(name) {
+ $('#fileInput').hide()
+ $('#name').css("display", "block")
+ $('#name').text(name)
+ $('#content').css("display", "block")
+}
--- src/worker.js
+++ src/worker.js
@@ -1,42 +1,39 @@
 export function jsonToTree(arr) {
   const tree = document.getElementById("tree");
 
-  let element = "ul";
   let mom = tree;
-  let add_class = false;
+  let index = 0;
 
-  arr.forEach(line => {
+  for(let i = 0; i < arr.length; i++) {
+    if(i == 0 || i == arr.length - 1)
+      continue
+
+    const line = arr[i]
+
+    if(line[line.length - 1] === '{' ||
+    line[line.length - 1] === '[') {
+ 
+      let ne = document.createElement('ul');
+      $(ne).css("list-style-type", "none")
+
+      mom.append(ne)
+      mom = ne
+    }
+
+    //Create the li
+
+    var el = document.createElement('li')
+    el.appendChild(document.createTextNode(`${line}`))
+    mom.append(el);
+  
+    //end
+
     if(line[line.length - 1] === '}' ||
-       line[line.length - 1] === ']' ||
-       line[line.length - 2] === '}' ||
-       line[line.length - 2] === ']' ) {
+      line[line.length - 1] === ']' ||
+      line[line.length - 2] === '}' ||
+      line[line.length - 2] === ']' ) {
 
       mom = mom.parentNode;
     }
-
-    if(line[line.length - 1] === '{' ||
-       line[line.length - 1] === '[') {
-    
-      element = "ul";
-      let ne = document.createElement('ul');
-      mom.appendChild(ne)
-      mom = ne
-
-      add_class = true;
-    }
-
-    var el = document.createElement('li')
-
-    if(add_class) {
-      $(el).addClass('test')
-      add_class = false;
-    }
-
-    el.appendChild(document.createTextNode(`${line}`))
-    mom.appendChild(el);
-
-    if(element == "ul")
-      element = "li";
-
-    });
+  }
 }
