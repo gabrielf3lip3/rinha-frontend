438d957f1b6192750d7e81b27a53578d76fea037
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2023-10-29 16:57:08 -0300

    remove comments

--- src/worker.js
+++ src/worker.js
@@ -14,36 +14,7 @@ self.addEventListener('message', (e) => {
 
 function jsonToTree(obj) {
   let tree = ""
-
   tree = processObj(obj, tree)
-  // let tree = ""
-  // const json = JSON.stringify(obj, null, 2)
-  // const arr = json.split("\n")
-
-  // arr.forEach(line => {
-  //   if(line[line.length - 1] === '}' ||
-  //     line[line.length - 1] === ']' ||
-  //     line[line.length - 2] === '}' ||
-  //     line[line.length - 2] === ']' ) {
-
-  //     tree += '</ul>'
-  //   }
-
-  //   const keyValue = line.split(":")
-  //   if(keyValue.length === 2) {
-  //     tree += `<li><span class="key">${keyValue[0]}: </span><span class="value">${keyValue[1]}</span></li>`
-  //   }
-  //   else {
-  //     tree += `<li><span class="single">${line}</span></li>`
-  //   }
-
-  //   if(line[line.length - 1] === '{' ||
-  //      line[line.length - 1] === '[') {
- 
-  //     tree += '<ul>'
-  //   }
-  // });
-
   return tree
 }
 
